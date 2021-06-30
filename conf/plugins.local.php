aa4e021961ca009359ac2f8cfae25f58bc75bb1a
Michael B WproxyM <wproxym@at.gmail>
2021-06-30 16:13:34 -0700

    disabled plugins

---
+++ conf/plugins.local.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Local plugin enable/disable settings
+ * Auto-generated through plugin/extension manager
+ *
+ * NOTE: Plugins will not be added to this file unless there is a need to override a default setting. Plugins are
+ *       enabled by default.
+ */
+$plugins['authpdo'] = 0;
+$plugins['docnavigation'] = 0;
+$plugins['popularity'] = 0;
+$plugins['autostart'] = 0;
+$plugins['authad'] = 0;
+$plugins['authldap'] = 0;
+$plugins['include'] = 0;
+$plugins['pagelist'] = 0;
